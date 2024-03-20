01b0bfb04ffa782eacbe4f4af4716913ee05c6be
steven <pramonosteven.cr2@gmail.com>
2024-03-20 02:19:45 +0000

    edit experiment_pipeline.py fix logging and workflow

--- experiment_pipeline.py
+++ experiment_pipeline.py
@@ -6,6 +6,7 @@ from typing import List, Tuple, Optional
 import json
 import torch
 from tqdm import tqdm
+import logging
 
 from torchvision.transforms import Compose, Normalize, Resize, InterpolationMode
 
@@ -16,38 +17,50 @@ from run_train import train_batch, train_log, save
 from train import preprocess_text
 
 
-class Config(object):
-    def __init__(self, initial_data):
-        for key in initial_data:
-            setattr(self, key, initial_data[key])
-            
-config = {
-    'train_cxr_folder': 'data/mimic-cxr-data/h5files/train-smoke-test/',
-    'train_txt_folder': 'data/mimic-cxr-data/reports/train-smoke-test/',
-    'val_cxr_filepath': 'data/chexpert-test/chexpert-test/chexlocalize/CheXpert/chexpert_test.h5',
-    'val_groundtruth': 'data/chexpert-test/groundtruth.csv',
-    'batch_size': 64,
-    'epochs': 10,
-    'lr': 5e-5,
-    'log_interval': 100,
-    'save_dir': 'data/chexzero-experiments/',
-    'seed': 1234,
-    'optimizer': 'sgd',
-    'momentum': 0.9,
-    'context_length': 77,
-    'random_init': False,
-    'model_name': 'experiment_SMOKE_TEST'
-}
-
-config = Config(config)
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--train_cxr_folder', type=str, default='data/mimic-cxr-data/h5files/train/', help="Directory to load chest x-ray image data from.")
+    parser.add_argument('--train_txt_folder', type=str, default='data/mimic-cxr-data/reports/train/', help="Directory to load radiology report impressions text from.")
+    parser.add_argument('--val_cxr_filepath', type=str, default='data/chexpert-test/chexpert-test/chexlocalize/CheXpert/chexpert_test.h5', help='.h5 file: validation images')
+    parser.add_argument('--val_groundtruth', type=str, default='data/chexpert-test/groundtruth.csv', help="CSV file: validation groundtruth")
+    parser.add_argument('--batch_size', type=int, default=16)
+    parser.add_argument('--epochs', type=int, default=4)
+    parser.add_argument('--lr', type=float, default=1e-4)
+    parser.add_argument('--val_interval', type=int, default=100, help="number of batch before validation is performed.")
+    parser.add_argument('--save_dir', type=str, default="data/chexzero-experiments/", help="Directory to save the trained model.")
+    parser.add_argument('--seed', type=int, default=1234)
+    parser.add_argument('--optimizer', type=str, default="sgd")
+    parser.add_argument('--momentum', type=float, default=0.9)
+    parser.add_argument('--context_length', type=int, default=77)
+    parser.add_argument('--random_init', action='store_true')
+    parser.add_argument('--model_name', type=str, default="model")
+    args = parser.parse_args()
+    return args
 
 
-cxr_labels: List[str] = ['Atelectasis','Cardiomegaly', 
-                                      'Consolidation', 'Edema', 'Enlarged Cardiomediastinum', 'Fracture', 'Lung Lesion',
-                                      'Lung Opacity', 'No Finding','Pleural Effusion', 'Pleural Other', 'Pneumonia', 
-                                      'Pneumothorax', 'Support Devices']
+# class Config(object):
+#     def __init__(self, initial_data):
+#         for key in initial_data:
+#             setattr(self, key, initial_data[key]) 
+# config = {
+#     'train_cxr_folder': 'data/mimic-cxr-data/h5files/train-smoke-test/',
+#     'train_txt_folder': 'data/mimic-cxr-data/reports/train-smoke-test/',
+#     'val_cxr_filepath': 'data/chexpert-test/chexpert-test/chexlocalize/CheXpert/chexpert_test.h5',
+#     'val_groundtruth': 'data/chexpert-test/groundtruth.csv',
+#     'batch_size': 64,
+#     'epochs': 10,
+#     'lr': 5e-5,
+#     'val_interval': 100,
+#     'save_dir': 'data/chexzero-experiments/',
+#     'seed': 1234,
+#     'optimizer': 'sgd',
+#     'momentum': 0.9,
+#     'context_length': 77,
+#     'random_init': False,
+#     'model_name': 'model'
+# }
+# config = Config(config)
 
-cxr_pair_template: Tuple[str] = ("{}", "no {}")
 
 
 def experiment_pipeline(
@@ -55,7 +68,13 @@ def experiment_pipeline(
     cxr_pair_template,
     config
 ): 
-    model_save_dir = os.path.join(config.save_dir, config.model_name)
+    
+    model_name = f"{config.model_name}_batchsize_{config.batch_size}_clength_{config.context_length}_opt_{config.optimizer}_lr_{config.lr}_momentum_{config.momentum}_seed_{config.seed}"
+    logger = logging.getLogger(__name__)
+    log_path = os.path.join(config.save_dir, model_name, 'log.log')
+    logging.basicConfig(filename=log_path, encoding='utf-8', level=logging.DEBUG)
+    
+    model_save_dir = os.path.join(config.save_dir, model_name)
     if not os.path.exists(model_save_dir): 
         # Create a new folder if not exists
         os.makedirs(model_save_dir)
@@ -82,7 +101,6 @@ def experiment_pipeline(
     # Run training
     example_ct = 0  # number of examples seen
     batch_ct = 0
-    report_freq = config.log_interval
     best_roc_auc = -np.inf
     
     train_total_batches = [len(train_loader) for train_loader in train_loaders]
@@ -91,6 +109,7 @@ def experiment_pipeline(
     batch_train_losses = []
     batch_val_roc_auc = []
     y_true = make_true_labels(cxr_true_labels_path=config.val_groundtruth, cxr_labels=cxr_labels)
+    
     
     for e, epoch in enumerate(range(config.epochs)):
         running_loss = 0.0 # running loss over batch
@@ -134,23 +153,26 @@ def experiment_pipeline(
                 with open(batch_val_roc_auc_path, 'w') as f:
                     json.dump(batch_val_roc_auc, f)
 
-                # Report metrics every `report_freq` batch
-                if (batch_ct % report_freq) == 0:
-                    train_log(running_loss / report_freq, example_ct, epoch)
+                # Report metrics every <val_interval> batch
+                if (batch_ct % config.val_interval) == 0:
+                    logger.info(f"epoch {e} batch {batch_ct} loss {running_loss/config.val_interval}")
                     running_loss = 0.0
                 
-                # save if AUC is higher than best-so-far
-                if avg_roc_auc > best_roc_auc:
-                    checkpoint_name = f"checkpoint_batch_{batch_ct}_batchsize_{config.batch_size}_auc_{avg_roc_auc}.pt"
-                    model_path = os.path.join(model_save_dir, checkpoint_name)
-                    print("Saved checkpoint to: ", model_path)
-                    save(model, model_path)
-                    best_roc_auc = avg_roc_auc
+                    # save if validation AUC is higher than best AUC
+                    if avg_roc_auc > best_roc_auc:
+                        checkpoint_name = f"checkpoint_batch_{batch_ct}_batchsize_{config.batch_size}_auc_{avg_roc_auc}.pt"
+                        model_path = os.path.join(model_save_dir, checkpoint_name)
+                        print("Saved checkpoint to: ", model_path)
+                        save(model, model_path)
+                        best_roc_auc = avg_roc_auc
                     
 
 if __name__ == "__main__":
-    experiment_pipeline(
-        cxr_labels,
-        cxr_pair_template,
-        config
-    )
+    cxr_labels: List[str] = ['Atelectasis','Cardiomegaly', 
+                                      'Consolidation', 'Edema', 'Enlarged Cardiomediastinum', 'Fracture', 'Lung Lesion',
+                                      'Lung Opacity', 'No Finding','Pleural Effusion', 'Pleural Other', 'Pneumonia', 
+                                      'Pneumothorax', 'Support Devices']
+    cxr_pair_template: Tuple[str] = ("{}", "no {}")
+    config = parse_args()
+    
+    experiment_pipeline(cxr_labels, cxr_pair_template, config)
